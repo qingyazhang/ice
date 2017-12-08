189ff6c51bab12dd43195cee2e4ca4e205017cdf
Joe George <joe@zeroc.com>
2017-12-08 15:28:35 -0500

    Fix MAKEFLAGS

--- scripts/generate-ci-dockerfile.sh
+++ scripts/generate-ci-dockerfile.sh
@@ -2,7 +2,7 @@
 
 cat << EOF > "$(git rev-parse --show-toplevel)/Dockerfile"
 FROM ${DOCKER_BASE_IMAGE}
-RUN make -j3 V=1 OPTIMIZE=no USER_NAMESPACES=no CONFIGS="shared cpp11-shared"
+RUN make ${MAKEFLAGS}"
 RUN find cpp/src -name build -exec rm -rf {} + \\
     && find cpp/test -name pie -exec rm -rf {} +
 EOF
