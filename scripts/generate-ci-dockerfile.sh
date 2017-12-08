6ee66b73649d10cbe1cdb3ef95f65d9ce7dfc91c
Joe George <joe@zeroc.com>
2017-12-08 14:24:15 -0500

    Travis fixes

--- scripts/generate-ci-dockerfile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cat << EOF > "$(git rev-parse --show-toplevel)/Dockerfile"
-ARG DOCKER_BASE_IMAGE
-FROM ${DOCKER_BASE_IMAGE}
-RUN make ${MAKEFLAGS}
-RUN find cpp/src -name build -exec rm -rf {} + \\
-    && find cpp/test -name pie -exec rm -rf {} +
-EOF
