217af969a054807315498abbe6f90351613d4386
Joe George <joe@zeroc.com>
2017-12-08 15:31:25 -0500

    Fix typo

--- scripts/generate-ci-dockerfile.sh
+++ scripts/generate-ci-dockerfile.sh
@@ -2,7 +2,7 @@
 
 cat << EOF > "$(git rev-parse --show-toplevel)/Dockerfile"
 FROM ${DOCKER_BASE_IMAGE}
-RUN make ${MAKEFLAGS}"
+RUN make ${MAKEFLAGS}
 RUN find cpp/src -name build -exec rm -rf {} + \\
     && find cpp/test -name pie -exec rm -rf {} +
 EOF
